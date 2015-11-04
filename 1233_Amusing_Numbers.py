665afdf1e7a867d8704a6121617f5eeb2b6d6d71
unknown <gggrinea@gmail.com>
2015-11-04 15:58:04 +0200

    1233 accepted

--- 1233_Amusing_Numbers.py
+++ 1233_Amusing_Numbers.py
@@ -1,8 +1,8 @@
 from sys import stdout, stdin
 
 input = stdin.readline().split()
-K = int(input[0])
-M = int(input[1])
+K = long(input[0])
+M = long(input[1])
 
 
 def brut_k(k):
@@ -15,10 +15,10 @@ def brut_k(k):
 def minimal_postion_k(k):
 	k_str = str(k)
 	k_str_len = len(k_str)
-	k_last = int(k_str[0])
+	k_last = long(k_str[0])
 	base1 = 0
 	for i in xrange(k_str_len):
-		base1 = base1 + int(k_str[0:i+1]) - 10**i
+		base1 = base1 + long(k_str[0:i+1]) - 10**i
 	base1 = base1 + i
 	return base1 + 1
 	
@@ -31,18 +31,20 @@ def minimal_postion_km(k, m):
 	elif (k == 1):
 		return 0
 	diff_m = m - min_pos
-	k1 = k - 1
-	k1_base = 10 ** (len(str(k)) - 1)
-	if(k1_base == k):
+	k1 = (k - 1) * 10 + 9
+	k1_base = 10 ** (len(str(k)))
+	if(k1_base == k*10):
 		return 0
 	tmp_diff_m = diff_m
 	pos = 0
+
 	while(tmp_diff_m > (k1 - k1_base + 1)):
 		tmp_diff_m = tmp_diff_m - (k1 - k1_base + 1)
 		k1 = k1 * 10 + 9
 		k1_base = k1_base * 10
-	pos = k1_base + tmp_diff_m
+	pos = k1_base + tmp_diff_m - 1
 
 	return pos
-
-print minimal_postion_km(K, M)
+t = minimal_postion_km(K, M)
+print t
+#print brut_k(t).index(str(K))+1
